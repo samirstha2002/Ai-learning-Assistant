d6c9e764f8674b45889d5bba9ff115a6be6910fd
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-29 15:37:04 +0545

    Dahboard page

--- frontend/ai-learning-assistant/src/components/layout/AppLayout.jsx
+++ frontend/ai-learning-assistant/src/components/layout/AppLayout.jsx
@@ -1,5 +1,25 @@
-function AppLayout() {
-  return <div>Hi</div>;
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { useState } from "react";
+
+function AppLayout({ children }) {
+  const [isSidebarOpen, setisSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setisSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="flex h-screen bg-neutral-50 text-neutral-900 ">
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className="flex-1 flex flex-col overflow-hidden ">
+        <Header toggleSidebar={toggleSidebar} />
+
+        <main className="flex-1 overflow-x-hidden overflow-y-auto p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default AppLayout;
